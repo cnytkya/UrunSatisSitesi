a2070d4933c9e91b3882c3dd9c6209b8bf3ac38f
Cüneyt Kaya <102078972+cnytkya@users.noreply.github.com>
2022-09-15 20:26:15 +0300

    Add project files.

---
+++ UrunSatisSitesi.WebUI/Areas/Admin/Controllers/AppUsersController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UrunSatisSitesi.Entities;
+using UrunSatisSitesi.Service.Repositories;
+
+namespace UrunSatisSitesi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AppUsersController : Controller
+    {
+        // GET: AppUsersController
+        private readonly IRepository<AppUser> _repository; // DI-Dependency injection yöntemiyle
+
+        public AppUsersController(IRepository<AppUser> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: AppUsersController
+        public ActionResult Index()
+        {
+            return View(_repository.GetAll());
+        }
+
+        // GET: AppUsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AppUsersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppUsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _repository.AddAsync(appUser);
+                    await _repository.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            return View(appUser);
+        }
+
+        // GET: AppUsersController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var kayit = await _repository.FindAsync(id);
+
+            if (kayit == null) return NotFound();
+
+            return View(kayit);
+        }
+
+        // POST: AppUsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _repository.Update(appUser);
+                    await _repository.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            return View(appUser);
+        }
+
+        // GET: AppUsersController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var kayit = await _repository.FindAsync(id);
+
+            if (kayit == null) return NotFound();
+
+            return View(kayit);
+        }
+
+        // POST: AppUsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, AppUser appUser)
+        {
+            try
+            {
+                _repository.Delete(appUser);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ UrunSatisSitesi.WebUI/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using UrunSatisSitesi.Data;
+using UrunSatisSitesi.Service.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+
+builder.Services.AddDbContext<DatabaseContext>(); //options => options.UseSqlServer() uygulamada dabasecontext imizde sql server kullanacaðýmýzý bildirdik
+builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>)); // Projede bir yerde IRepository interfaci kullanýlmak istenirse, Repository nesnesinden bir örnek oluþtur ve kullanýma sun.
+// Dependency injection yöntemi olarak 3 farklý yöntemimiz var;
+// 1-AddSingleton : Oluþturmasý istenen nesneden uygulama çalýþtýðýnda 1 tane oluþtururu ve her istekte bu nesneyi gönderir.
+// 2-AddTransient : Oluþturmasý istenen nesne için gelen her istekte yani bir nesne oluþturur.
+// 3-AddScoped : Oluþturmasý istenen nesne için gelen isteðe bakarak eðer daha önce oluþmuþ bir örnek varsa geriye onu döner, yoksa yeni nesne oluþturup döner.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSession();
+
+app.UseAuthentication(); // oturum açma iþlemi yapacaðýz admin için
+app.UseAuthorization(); // Authorization : yetkilendirme, rol vb iþlemler için
+
+app.MapControllerRoute(
+            name: "admin",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
